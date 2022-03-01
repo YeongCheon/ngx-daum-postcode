f00609f731a94f4948e487d6b45a6f0ea476e31d
YeongCheon Kim <kyc1682@gmail.com>
2022-03-01 09:34:16 +0900

    우편번호 서비스 embed 형식 호출 기능 및 이벤트 바인딩

--- projects/example/src/app/app.component.ts
+++ projects/example/src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import {
+  DaumPostcode,
+  Search,
+  Size,
+} from 'projects/ngx-daum-postcode/src/public-api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'example';
+  title = 'ngxDaumPostcode Example';
+
+  onComplete(daumPostcode: DaumPostcode): void {
+    console.debug(daumPostcode);
+  }
+
+  onResize(size: Size): void {
+    console.debug(size);
+  }
+
+  onSearch(data: Search): void {
+    console.debug(data);
+  }
 }
--- projects/ngx-daum-postcode/src/lib/ngx-daum-postcode.component.ts
+++ projects/ngx-daum-postcode/src/lib/ngx-daum-postcode.component.ts
@@ -1,4 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  Inject,
+  OnInit,
+  Output,
+  Renderer2,
+} from '@angular/core';
+
+declare let daum: any;
 
 @Component({
   selector: 'ngx-daum-postcode',
@@ -6,7 +17,96 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ngx-daum-postcode.component.scss'],
 })
 export class NgxDaumPostcodeComponent implements OnInit {
-  constructor() { }
+  @Output('onComplete')
+  onComplete = new EventEmitter<DaumPostcode>();
 
-  ngOnInit(): void { }
+  @Output('onResize')
+  onResize = new EventEmitter<Size>();
+
+  @Output('onSearch')
+  onSearch = new EventEmitter<Search>();
+
+  constructor(
+    @Inject(DOCUMENT) private document: Document,
+    private renderer: Renderer2,
+    private hostElement: ElementRef
+  ) { }
+
+  ngOnInit(): void {
+    const src = '//t1.daumcdn.net/mapjsapi/bundle/postcode/prod/postcode.v2.js';
+    const scriptElement = this.renderer.createElement('script');
+    scriptElement.type = 'text/javascript';
+    scriptElement.src = src;
+    scriptElement.onload = () => {
+      this.initDaumPostcode();
+    };
+
+    this.renderer.appendChild(this.document.body, scriptElement);
+  }
+
+  private initDaumPostcode() {
+    new daum.Postcode({
+      oncomplete: (data: DaumPostcode) => {
+        this.onComplete.emit(data);
+      },
+      onresize: (data: Size) => {
+        this.onResize.emit(data);
+      },
+      onsearch: (data: Search) => {
+        this.onSearch.emit(data);
+      },
+    }).embed(this.hostElement.nativeElement);
+  }
+}
+
+export interface Size {
+  width: number;
+  height: number;
+}
+
+export interface Search {
+  q: string;
+  count: number;
+}
+
+export interface DaumPostcode {
+  postcode: string;
+  postcode1: string;
+  postcode2: string;
+  postcodeSeq: string;
+  zonecode: string;
+  address: string;
+  addressEnglish: string;
+  addressType: string;
+  bcode: string;
+  bname: string;
+  bnameEnglish: string;
+  bname1: string;
+  bname1English: string;
+  bname2: string;
+  bname2English: string;
+  sido: string;
+  sidoEnglish: string;
+  sigungu: string;
+  sigunguEnglish: string;
+  sigunguCode: string;
+  userLanguageType: string;
+  query: string;
+  buildingName: string;
+  buildingCode: string;
+  apartment: string;
+  jibunAddress: string;
+  jibunAddressEnglish: string;
+  roadAddress: string;
+  roadAddressEnglish: string;
+  autoRoadAddress: string;
+  autoRoadAddressEnglish: string;
+  autoJibunAddress: string;
+  autoJibunAddressEnglish: string;
+  userSelectedType: string;
+  noSelected: string;
+  hname: string;
+  roadnameCode: string;
+  roadname: string;
+  roadnameEnglish: string;
 }
